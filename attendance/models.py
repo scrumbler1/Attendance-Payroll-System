151aca2baef6d003ff889c9a60c2e03b5399a787
Erish <irishmjn@gmail.com>
2026-01-05 17:50:14 +0545

    Feat: updated code for user attendance and record of leave

--- attendance/models.py
+++ attendance/models.py
@@ -25,7 +25,7 @@ class Department(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)#this cannot be changed when the object is changed later
     updated_at = models.DateTimeField(auto_now=True)
-    work_start_time = models.TimeField(defailt="09:00")
+    work_start_time = models.TimeField(default="09:00")
     work_end_time = models.TimeField(default = "17:00")
     working_days_per_week = models.IntegerField(default=6)
     
@@ -65,3 +65,127 @@ class Employee(models.Model):
     gender=models.CharField(max_length=10, choices=GENDER_CHOICES)
     date_of_birth = models.DateField(null=False,blank=False)
     department=models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    designation=models.CharField(max_length=200, null=False)
+    employment_type=models.CharField(max_length=20, choices=EMPLOYMENT_TYPE, default='FULL_TIME')
+    date_joined=models.DateTimeField(auto_now_add=True)
+    date_left=models.DateField(null=True, blank=True)
+    is_active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now= True)
+    location=models.CharField(max_length=25, blank=True)
+    is_offsite = models.BooleanField(default=False)
+    is_wfh_enabled=models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+class AttendanceRecord(models.Model):
+        ATTENDANCE_CHOICES = [
+            ('present', 'Present'),
+            ('half_day', 'Half Day'),
+            ('on_leave', 'On Leave'),
+            ('unpaid_leave', 'Unpaid leave'),
+            ('holiday', 'Holiday'),
+            ('weekend', 'Weekend'),
+            ('absent', 'Absent')
+        ]
+
+        employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="attendance_records")
+        date=models.DateField(default=timezone.now)
+        check_in=models.DateTimeField(null=True, blank=True)
+        status= models.CharField(max_length=20, choices = ATTENDANCE_CHOICES, default='absent')
+        hours_worked=models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+        late_minutes=models.IntegerField(default=0)
+        overtime_hours=models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+        remarks=models.TextField(blank=True, null=True)
+        is_auto_checkout=models.BooleanField(default=False)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at =models.DateTimeField(auto_now=True)
+
+        class Meta:
+            unique_together=('employee', 'date') #one employee per day record
+
+        def calculate_status_and_hours(self):
+            #skipping the fixed status
+            """this logic states that if some employee is in these condition they will be marked as absent"""
+            if self.status in {'on_leave', 'unpaid_leave', 'holiday', 'weekend'}:
+                self.hours_worked = 0
+                self.hours_minutes = 0
+                self.overtime_hours = 0
+            if not self.check_in or not self.check_out:
+                self.status = 'absent'
+                self.hours_worked = 0
+                self.hours_minutes = 0
+                self.overtime_hours = 0
+                return
+            dept = self.employee.department
+            if not dept:
+                self.status = 'absent'
+                return
+            
+            check_in = make_aware_if_naive(self.check_in)
+            check_out = make_aware_if_naive(self.check_out)
+            shift_start = make_aware_if_naive(
+                datetime.combine(self.date, dept.work_start_time)
+            )
+            shift_end = make_aware_if_naive(
+                datetime.combine(self.date, dept.work_end_time)
+            )
+            effective_checkout = min(check_out, shift_end)
+            worked_minutes = int(
+                max(0, (effective_checkout- check_in).total_seconds() // 60)
+            )
+            self.hours_worked = round(worked_minutes / 60,2)
+            if worked_minutes >= 480:
+                self.status = 'present'
+            elif worked_minutes >=240:
+                self.status = 'half_day'
+            else:
+                self.status = 'absent'
+            #the logic that handles the late minutes arrival
+            if self.status in {'present', 'half_day'}:
+                if check_in > shift_start:
+                    late = int((check_in - shift_start).total_seconds() // 60)
+                    #TODO: check the grace minutes in department and set it otherwise it will create the bug of not having grace minutes
+                    grace = getattr(dept, 'grace_minutes', 0)
+                    self.late_minutes = max(0, late - grace)
+                else:
+                    self.late_minutes = 0
+            else:
+                self.late_minutes = 0
+
+            """Overtime logic"""
+            if check_out > shift_end:
+                overtime_minutes = int((check_out-shift_end).total_seconds() // 60)
+                self.overtime_hours = round(overtime_minutes / 60, 2)
+            else:
+                self.overtime_hours = 0
+
+        def __str__(self):
+            return f"{self.employee.user.username}-{self.date}-{self.status}"
+class LeaveRequest(models.Model):
+    LEAVE_TYPE = [
+        ('annual', 'Annual Leave'),
+        ('sick', 'Sick Leave'),
+        ('casual', 'Casual Leave'),
+        ('maternity', 'Maternity Leave'),
+        ('paternity', 'Paternity Leave'),
+        ('unpaid', 'Unpaid Leave')
+    ]
+    LEAVE_STATUS = [
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected') 
+    ]
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="leave_requests")
+    leave_type = models.CharField(max_length=50, choices=LEAVE_TYPE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    reason=models.TextField(blank=True)
+    status =models.CharField(max_length=20, choices=LEAVE_STATUS, default='pending')
+    approved_by = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True, related_name='approved_leaves')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.employee.user.username} - {self.leave_type} - {self.start_date} to {self.end_date}"
